4fd7de6c7e77ab06b037311d471e0c206aa04737
MadeByForeV <yemreucmzz@gmail.com>
2022-06-24 09:58:39 +0300

    Removed userId

--- index.js
+++ index.js
@@ -1,17 +1,6 @@
 async function load(url) {
   const response = await fetch(url).then((res) => res.json());
   let dataTable = document.querySelector("table").querySelector("tbody");
-  //   response.media.data.map((item) => {
-  //     async function loadMedia(url) {
-  //       const media = await fetch(url).then((response) => response.json());
-  //       console.log(media);
-
-  //     }
-  //     // dataTable.innerHTML = `dataTable + item.id`;
-  //     loadMedia(
-  //       `https://graph.instagram.com/${item.id}?fields=id,media_type,media_url,username,timestamp,caption&access_token=${token}`
-  //     );
-  //   });
 
   var html = response.media.data.map(function (item) {
     async function loadMedia(url) {
@@ -32,8 +21,6 @@ async function load(url) {
 }
 
 const token = ""; // your token
-
-const userId = ""; // your user id
 
 load(
   "https://graph.instagram.com/me?fields=id,username,media&access_token=" +
