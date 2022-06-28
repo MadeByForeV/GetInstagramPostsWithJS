a84daaa6c9ce21111a0dee9bc99eeac863cde7e9
MadeByForeV <yemreucmzz@gmail.com>
2022-06-28 15:04:06 +0300

    Bugfix

--- index.js
+++ index.js
@@ -2,21 +2,25 @@ async function load(url) {
   const response = await fetch(url).then((res) => res.json());
   let dataTable = document.querySelector("table").querySelector("tbody");
 
-  var html = response.media.data.map(function (item) {
-    async function loadMedia(url) {
-      const media = await fetch(url).then((response) => response.json());
-      console.log(media);
-      dataTable.innerHTML =
-        dataTable.innerHTML +
-        ` <div>
-      <div>Açıklama : ${media.caption}</div>
-      <img class="img-fluid" width="50" height="50" src="${media.media_url}" />
-      </div>`;
+  console.log(response.media.data);
+
+  var html = response.media.data.map((item, index) => {
+    if (index < 999999) {
+      async function loadMedia(url) {
+        const media = await fetch(url).then((response) => response.json());
+        console.log(media);
+        dataTable.innerHTML =
+          dataTable.innerHTML +
+          ` <div>
+        <div>Açıklama : ${media.caption}</div>
+        <img class="img-fluid" width="500" height="500" src="${media.media_url}" />
+        </div>`;
+      }
+      // dataTable.innerHTML = `dataTable + item.id`;
+      loadMedia(
+        `https://graph.instagram.com/${item.id}?fields=id,media_type,media_url,username,timestamp,caption&access_token=${token}`
+      );
     }
-    // dataTable.innerHTML = `dataTable + item.id`;
-    loadMedia(
-      `https://graph.instagram.com/${item.id}?fields=id,media_type,media_url,username,timestamp,caption&access_token=${token}`
-    );
   });
 }
 
